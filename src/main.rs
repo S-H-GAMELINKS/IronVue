abc8b75091dfd0dd80cd907ffb544cb2d9f162bf
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-11 13:40:11 +0900

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello world!")))
+    }).http("localhost:3000").unwrap();
 }
