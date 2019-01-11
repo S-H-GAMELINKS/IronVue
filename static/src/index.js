0434d1a8b8a89a70f322d5c8ae670ec82f76f2b4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-11 14:20:07 +0900

    import & use Header.vue

--- static/src/index.js
+++ static/src/index.js
@@ -2,10 +2,15 @@ import Vue from 'vue';
 import * as BootstrapUmi from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 
+import Header from '../components/layouts/Header.vue';
+
 Vue.use(BootstrapUmi);
 
 const app = new Vue({
     el: ".app",
+    components: {
+        'nav-bar': Header
+    },
     data: function() {
         return {
             text: "Hello Iron & Vue.js"
