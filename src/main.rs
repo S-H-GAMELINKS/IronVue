63dd0307bb62b1c247722eb9627f080233a08bc3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-11 17:20:25 +0900

    get server port

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ use iron::prelude::*;
 use staticfile::Static;
 use mount::Mount;
 use std::path::Path;
+use std::env;
+
+fn get_server_port() -> u16 {
+    env::var("PORT").ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(3000)
+}
 
 fn main() {
     let mut mount = Mount::new();
@@ -18,5 +25,5 @@ fn main() {
 
     mount.mount("/index.js", Static::new(Path::new("static/index.js")));
 
-    Iron::new(mount).http(env!("PORT")).unwrap();
+    Iron::new(mount).http(("0.0.0.0", get_server_port())).unwrap();
 }
