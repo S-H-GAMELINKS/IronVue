5e2023bd6954c2dbbc368b288894c1d43492d0f3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-11 14:15:41 +0900

    fix usinf vue-somponent for webpack

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
                 test: /\.css/,
                 use: [
                     'style-loader',
